a8860ad729f176ccce5b7ecb68c734226c34baf2
andr0med0s <andromedoz@gmail.com>
2023-02-22 21:20:27 +0500

    lesson 38 Calculator

--- src/HelloWorld/src/Family.java
+++
@@ -1,9 +0,0 @@
-package HelloWorld.src;
-
-public class  Family {
-    public static void main(String[] args) {
-        System.out.println("Мама: " + "Эмма");
-        System.out.println("Бабушка: " + "Анна");
-        System.out.println("Моё имя: " + "Андрей");
-    }
-}
--- src/HelloWorld/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package HelloWorld.src;
-
-public class HelloWorld {
-    public static void main(String[] args) { // точка входа
-
-        System.out.println("Hello World"); // вызов метода, метод который что-то печатает в консоль
-
-    }
-}
